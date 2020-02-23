407af7acee659afe72240a8c2066e5b1f2d41c29
Luiz Henrique Carneiro Gonçalves <goncalves.luizhc@gmail.com>
2020-02-23 15:45:34 -0300

    Payment, one-to-one association

--- src/main/java/com/luizhcgoncalves/course/config/TestConfig.java
+++ src/main/java/com/luizhcgoncalves/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.luizhcgoncalves.course.entities.Category;
 import com.luizhcgoncalves.course.entities.Order;
 import com.luizhcgoncalves.course.entities.OrderItem;
+import com.luizhcgoncalves.course.entities.Payment;
 import com.luizhcgoncalves.course.entities.Product;
 import com.luizhcgoncalves.course.entities.User;
 import com.luizhcgoncalves.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
